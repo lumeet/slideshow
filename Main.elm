26d066cb3fd4b36ec1e4f2cc61505a4be99b4ef0
Teemu <teemu@aikais.in>
2017-02-21 21:30:03 +0200

    Add initial back button

--- Main.elm
+++ Main.elm
@@ -2,7 +2,8 @@ module Main exposing (main)
 
 import Html exposing (Html, div, text)
 import Html.Attributes exposing (style)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onWithOptions)
+import Json.Decode as Decode
 import Array
 import Slides exposing (slides)
 
@@ -17,6 +18,7 @@ type alias Slide =
 
 type Msg
     = Next
+    | Previous
 
 
 emptySlide =
@@ -24,16 +26,11 @@ emptySlide =
 
 
 model =
-    { slide =
-        case Array.get 0 slides of
-            Just slide ->
-                slide
-
-            Nothing ->
-                emptySlide
-    , currentNo = 1
-    , slideCount = Array.length slides
-    }
+    let
+        initial =
+            updateSlideAt 0
+    in
+        { initial | slideCount = Array.length slides }
 
 
 update msg model =
@@ -41,14 +38,43 @@ update msg model =
         Next ->
             model |> nextSlide
 
+        Previous ->
+            model |> previousSlide
+
 
 nextSlide model =
-    case Array.get model.currentNo slides of
+    let
+        slideNo =
+            case model.currentNo of
+                Just num ->
+                    num + 1
+
+                Nothing ->
+                    0
+    in
+        updateSlideAt slideNo
+
+
+previousSlide model =
+    let
+        slideNo =
+            case model.currentNo of
+                Just num ->
+                    num - 1
+
+                Nothing ->
+                    (Array.length slides) - 1
+    in
+        updateSlideAt slideNo
+
+
+updateSlideAt slideNo =
+    case Array.get slideNo slides of
         Just slide ->
-            { model | slide = slide, currentNo = model.currentNo + 1 }
+            { model | slide = slide, currentNo = Just slideNo }
 
         Nothing ->
-            { model | slide = emptySlide, currentNo = 0 }
+            { model | slide = emptySlide, currentNo = Nothing }
 
 
 bodyStyle =
@@ -60,12 +86,31 @@ bodyStyle =
     ]
 
 
+backLinkStyle =
+    [ ( "width", "100px" )
+    , ( "height", "100%" )
+    , ( "position", "fixed" )
+    , ( "top", "0" )
+    , ( "left", "0" )
+    , ( "background", "black" )
+    , ( "color", "white" )
+    ]
+
+
 view model =
     div
         [ style bodyStyle
         , onClick Next
         ]
-        [ div [] model.slide.content ]
+        [ div [] model.slide.content
+        , div
+            [ style backLinkStyle
+            , onWithOptions "click"
+                { preventDefault = True, stopPropagation = True }
+                (Decode.succeed Previous)
+            ]
+            [ text "<-" ]
+        ]
 
 
 main =
